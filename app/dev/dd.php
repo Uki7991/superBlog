81d13fb6e2127382dd82c1135445a34dc2003289
uki7991 <tilek.kubanov@gmail.com>
2018-03-22 14:53:45 +0600

    Registered bundles: fos_user, easy_admin Configured Entities (70%)

---
+++ app/dev/dd.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kubanov
+ * Date: 3/22/18
+ * Time: 12:52 PM
+ */
+
+class Dd
+{
+    public function dd($text)
+    {
+        dump($text);
+        die();
+    }
+}
