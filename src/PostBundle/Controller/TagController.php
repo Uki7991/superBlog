6c878b8541079cc1a5ed0df4f7af0ebad31b20dd
uki7991 <tilek.kubanov@gmail.com>
2018-03-29 17:16:33 +0600

    links to show tag

---
+++ src/PostBundle/Controller/TagController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace PostBundle\Controller;
+
+use PostBundle\Entity\Tag;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Class TagController
+ *
+ */
+class TagController extends Controller
+{
+    /**
+     * @Configuration\Route("/tag/{slug}", name="tag_show")
+     * @Configuration\Method("GET")
+     *
+     * @param $slug
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function show($slug)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $tagRepo = $em->getRepository('PostBundle:Tag');
+        $catRepo = $em->getRepository('PostBundle:Category');
+
+        $categories = $catRepo->findAll();
+        $tags = $tagRepo->findAll();
+        $activeTag = $tagRepo->findOneBy(['slugName' => $slug]);
+        $posts = $activeTag->getPosts();
+        $bigTag = $tagRepo->findBigTag();
+
+        return $this->render('@Post/tag/show.html.twig', [
+            'tags' => $tags,
+            'categories' => $categories,
+            'active' => $activeTag,
+            'posts' => $posts,
+            'bigTag' => $bigTag['counts'],
+        ]);
+    }
+}
---
+++ src/PostBundle/Tests/Controller/TagControllerTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace PostBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class TagControllerTest extends WebTestCase
+{
+}
