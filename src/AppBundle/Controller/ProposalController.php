286cc74e2c9c6692a1199de19d7119a19a12bcdb
uki7991 <tilek.kubanov@gmail.com>
2018-04-09 13:44:48 +0600

    Google api map

--- src/AppBundle/Controller/ProposalController.php
+++ src/AppBundle/Controller/ProposalController.php
@@ -2,14 +2,44 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Proposal;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
 
 class ProposalController extends Controller
 {
+    /**
+     * @Route("/proposal")
+     *
+     * @Method("POST")
+     *
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
     public function store(Request $request)
     {
-        
+        $message = [
+            'status' => 'error',
+        ];
+        $data = $request->request->all();
+
+        if (!$data) {
+            return $this->json($message);
+        }
+
+        $em = $this->getDoctrine()->getManager();
+
+        $proposal = new Proposal();
+        $proposal->setName($data['name']);
+        $proposal->setPhone($data['phone']);
+
+        $em->persist($proposal);
+        $em->flush();
+
+        $message['status'] = 'success';
+
+        return $this->json($message);
     }
 }
